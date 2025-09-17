4f7b97e1699f322b89d9578d8d864d408089c4d6
Sol Kim <defreturnnone@gmail.com>
2025-09-17 13:26:23 +0900

    add param module

---
+++ python/fone/core/param.py
@@ -0,0 +1,154 @@
+from .. import exceptions
+
+
+FoneParamTypeBool = 0
+FoneParamTypeInt = 1
+FoneParamTypeFloat = 2
+FoneParamTypeStr = 3
+
+
+class FoneParamBase(object):
+    def __init__(self, name, default):
+        super(FoneParamBase, self).__init__()
+        self.__name = name
+        self.__value = None
+        self.__default = None
+
+        self.set(default)
+        self.__default = self.__value
+
+    def name(self):
+        return self.__name
+
+    def default(self):
+        return self.__default
+
+    def get(self):
+        return self.__value
+
+    def set(self, value):
+        if not self.isValid(value):
+            raise exceptions.FoneInvalidParamValueError(self, value)
+
+        self.__value = value
+
+    def type(self):
+        raise exceptions.FoneNotImplementedError(self, "type")
+
+    def isValid(self, value):
+        raise exceptions.FoneNotImplementedError(self, "isValid")
+
+    def copy(self):
+        raise exceptions.FoneNotImplementedError(self, "copy")
+
+
+class FoneParamBool(FoneParamBase):
+    def __init__(self, name, default=False):
+        super(FoneParamBool, self).__init__(name, default)
+
+    def type(self):
+        return FoneParamTypeBool
+
+    def isValid(self, value):
+        return isinstance(value, bool)
+
+    def copy(self):
+        n = FoneParamBool(self.name(), self.default())
+        n.set(self.get())
+
+        return n
+
+
+class FoneParamStr(FoneParamBase):
+    def __init__(self, name, default="", valueList=None, enforceValueList=False):
+        self.__value_list = list(valueList) if isinstance(valueList, (list, tuple, set)) else []
+        self.__enforce_value_list = enforceValueList if self.__value_list else False
+
+        super(FoneParamStr, self).__init__(name, default)
+
+    def type(self):
+        return FoneParamStr
+
+    def enforceValueList(self):
+        return self.__enforce_value_list
+
+    def valueList(self):
+        return self.__value_list[:]
+
+    def isValid(self, value):
+        if not instance(value, str):
+            return False
+
+        if self.__enforce_value_list and value not in self.__value_list:
+            return False
+
+        return True
+
+    def copy(self):
+        n = FoneParamStr(self.name(), default=self.default(), valueList=self.__value_list, enforceValueList=self.__enforce_value_list)
+        n.set(self.get())
+
+        return n
+
+
+class FoneNumericParam(object):
+    def __init__(self, name, default=None, min=None, max=None):
+        self.__min = min
+        self.__max = max
+
+        super(FoneNumericParam, self).__init__(name, default)
+
+    def min(self):
+        return self.__min
+
+    def max(self):
+        return self.__max
+
+    def isValid(self):
+        if self.__min is not None and v < self.__min:
+            return False
+
+        if self.__max is not None and v > self.__max:
+            return False
+
+        return True
+
+    def copy(self):
+        n = self.__class__(self.name(), default=self.default(), min=self.__min, max=self.__max)
+        n.set(self.get())
+
+        return n
+
+
+class FoneParamInt(FoneNumericParam):
+    def __init__(self, name, default=0, min=None, max=None):
+        super(FoneParamInt, self).__init__(name, default=default, min=min, max=max)
+
+    def type(self):
+        return FoneParamTypeInt
+
+    def isValid(self, value):
+        if not isinstance(value, int):
+            return False
+
+        if isinstance(value, bool):
+            return False
+
+        return super(FoneParamInt, self).isValid(value)
+
+
+class FoneParamFloat(FoneNumericParam):
+    def __init__(self, name, default=0.0, min=None, max=None):
+        super(FoneParamFloat, self).__init__(name, default=default, min=min, max=max)
+
+    def type(self):
+        return FoneParamTypeFloat
+
+    def isValid(self, value):
+        if not isinstance(value, int):
+            return False
+
+        if isinstance(value, bool):
+            return False
+
+        return super(FoneParamFloat, self).isValid(value)
--- python/fone/exceptions.py
+++ python/fone/exceptions.py
@@ -11,3 +11,8 @@ class FoneNotImplementedError(Exception):
 class FoneIndexError(Exception):
     def __init__(self, index, total):
         super(FoneIndexError, self).__init__(f"list index out of range : {index}/{total}")
+
+
+class FoneInvalidParamValueError(Exception):
+    def __init__(self, instance, value):
+        super(FoneInvalidParamValueError, self).__init__(f"Invalid value '{value}' given for '{instance.__class__.__name__}'")
